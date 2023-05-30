2a9933ee93f910a5c720ee039925c982ad3efc47
BradWithGithub <91183171+awrdrfs@users.noreply.github.com>
2023-05-30 22:46:05 +0800

    Add files via upload

---
+++ hw5/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import shoppingList from './components/shoplist.vue'
+import rbutton from './components/Rbutton.vue'
+import fooditem from './components/fooditem.vue'
+import aform from './components/form.vue'
+import adata from './components/data.vue'
+</script>
+
+<template>
+  <shoppingList />
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ hw5/src/components/shoplist.vue
@@ -0,0 +1,73 @@
+<template>
+  <h1>請按照順序輸入ID</h1>
+  <div id="app">
+    <form v-on:submit.prevent="addItem">
+      <p>
+        ID?
+        <input type="number" required placeholder="item ID.." v-model="itemId">
+      </p>
+      <p>
+        Year?
+        <input type="number" placeholder="Year of items.." v-model="itemYear">
+      </p>
+      <p>
+        Price?
+        <input type="number" placeholder="Price of items.." v-model="itemPrice">
+      </p>
+      <button type="submit">Submit</button>
+    </form>
+
+    <div>
+      <h1>高筋麵粉價格資料(22kg)</h1>
+      <table>
+        <thead>
+        <tr>
+          <th>id</th>
+          <th>year</th>
+          <th>price</th>
+        </tr>
+        </thead>
+      </table>
+      <ul>
+        <li v-for="item in shoppingList">{{ item.id }}, {{ item.year}}, {{ item.price}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      itemID: null,
+      itemYear: null,
+      itemPrice: null,
+      shoppingList: [
+        { id: 1, year: 2023,  price:760}
+      ]
+    }
+  },
+  methods: {
+    addItem(){
+      let item = {
+        id: this.itemId,
+        year: this.itemYear,
+        price: this.itemPrice
+      }
+      this.shoppingList.push(item)
+      this.itemId = null
+      this.itemYear = null
+      this.itemPrice = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  border: dashed black 1px;
+  width: 200px;
+  padding: 20px;
+}
+</style>
